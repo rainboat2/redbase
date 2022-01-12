f3e4c8f0722e765be7f492b342b6a3cc82eab3fe
秦雨舟 <rainboatqin@gmail.com>
2022-01-12 23:39:56 +0800

    add gtest

---
+++ PF_Test.cpp
@@ -0,0 +1,31 @@
+#include "PF_BufferStrategy.h"
+#include "PF_BufferManger.h"
+#include "gtest/gtest.h"
+
+namespace{
+
+class PF_BufferStrategyTest : public testing::Test {
+protected:
+    void SetUp() override{
+        LRU_ = new LRU<BufferKey, BufferKeyHash>();
+    }
+
+    void TearDown() override{
+        // TODO: 删除LRU指针会导致segment fault
+        // delete LRU_;
+    } 
+
+protected:
+    BufferStrategy<BufferKey>* LRU_;
+};
+
+TEST_F(PF_BufferStrategyTest, LRU_test){
+    BufferKey b1{1, 2}, b2{234434, 0}, b3{2323, 4546};
+    EXPECT_TRUE(LRU_->empty());
+    LRU_->push(b1);
+    LRU_->push(b3);
+    LRU_->push(b2);
+    EXPECT_EQ(LRU_->size(), 3);
+}
+
+}

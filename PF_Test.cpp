cf2a628cdc9ef50ba82da7a6e3d5de300eefd004
秦雨舟 <rainboatqin@gmail.com>
2022-01-13 15:31:54 +0800

    fix the bug that delete LRU will cause segement fault

--- PF_Test.cpp
+++ PF_Test.cpp
@@ -22,10 +22,10 @@ protected:
 TEST_F(PF_BufferStrategyTest, LRU_test){
     BufferKey b1{1, 2}, b2{234434, 0}, b3{2323, 4546};
     EXPECT_TRUE(LRU_->empty());
-    // LRU_->push(b1);
-    // LRU_->push(b3);
-    // LRU_->push(b2);
-    // EXPECT_EQ(LRU_->size(), 3);
+    LRU_->push(b1);
+    LRU_->push(b3);
+    LRU_->push(b2);
+    EXPECT_EQ(LRU_->size(), 3);
 }
 
 }

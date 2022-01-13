08d8fc207fcb1e8a78a62f48d118035e0899bc97
秦雨舟 <rainboatqin@gmail.com>
2022-01-13 18:45:43 +0800

    add some test case for LRU in PF_BufferStrategy.h

--- PF_Test.cpp
+++ PF_Test.cpp
@@ -1,55 +1,61 @@
-#include "PF_BufferStrategy.h"
 #include "PF_BufferManager.h"
+#include "PF_BufferStrategy.h"
 #include "gtest/gtest.h"
-#include <iostream>
 #include <array>
+#include <iostream>
 
 class LRUTest : public testing::Test {
 protected:
-    void SetUp() override{
+    void SetUp() override
+    {
         LRU_ = new LRU<BufferKey, BufferKeyHash>();
-        keys[0] = {0, 0};
-        keys[1] = {0, 2147483647};
-        keys[2] = {2147483647, 0};
-        for (int i = 3; i < keys.size(); i++){
-            keys[i] = {i, i};
+        keys[0] = { 0, 0 };
+        keys[1] = { 0, 2147483647 };
+        keys[2] = { 2147483647, 0 };
+        for (int i = 3; i < keys.size(); i++) {
+            keys[i] = { i, i };
         }
     }
 
-    void pushAllKeys(){
-        for (auto &key: keys)
+    void pushAllKeys()
+    {
+        for (auto& key : keys)
             LRU_->push(key);
     }
 
-    void TearDown() override{
+    void TearDown() override
+    {
         delete LRU_;
-    } 
+    }
 
 protected:
     BufferStrategy<BufferKey>* LRU_;
     std::array<BufferKey, 40> keys;
 };
 
-TEST_F(LRUTest, LRU_PUSH_TEST){
+TEST_F(LRUTest, LRU_PUSH_TEST)
+{
     EXPECT_TRUE(LRU_->empty());
     pushAllKeys();
     EXPECT_EQ(LRU_->size(), keys.size());
 }
 
-TEST_F(LRUTest, LRU_POP_TEST){
+TEST_F(LRUTest, LRU_POP_TEST)
+{
     pushAllKeys();
     EXPECT_EQ(LRU_->pop(), keys[0]);
-    for (int i = 1; i < keys.size() - 1; i++){
+    for (int i = 1; i < keys.size() - 1; i++) {
         LRU_->visit(keys[i]);
     }
     EXPECT_EQ(LRU_->pop(), keys.back());
-    for (int i = 2; i < keys.size() - 1; i++){
+    for (int i = 2; i < keys.size() - 1; i++) {
         LRU_->visit(keys[i]);
     }
     EXPECT_EQ(LRU_->pop(), keys[1]);
 }
 
-TEST_F(LRUTest, LRU_CONTAIN_REMOVE_TEST){
+TEST_F(LRUTest, LRU_CONTAIN_REMOVE_TEST)
+{
     LRU_->push(keys[0]);
     EXPECT_TRUE(LRU_->contain(keys[0]));
     LRU_->remove(keys[0]);
@@ -57,7 +63,9 @@ TEST_F(LRUTest, LRU_CONTAIN_REMOVE_TEST){
     EXPECT_TRUE(LRU_->empty());
 }
 
-
-class BufferManagerTest : public testing::Test{
-
+class BufferManagerTest : public testing::Test {
+    void SetUp() override
+    {
+        
+    }
 }
